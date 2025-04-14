83b76ba18e37fa3acd356862fb2c6c612b15c78b
preshitkhoyani <hinagirish0306@gmail.com>
2025-04-14 16:56:19 +0530

    Add files via upload

---
+++ opertor_3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int x, y  ;
+	
+	printf(" eanter valu of x : ");
+	
+	scanf("%d" , & x);
+	
+	printf(" eanter valu of y : ");
+	
+	scanf("%d" , & y);
+	int ans = (x-y)*(x-y);
+	
+	printf("ans :%d" ,  ans );
+}
